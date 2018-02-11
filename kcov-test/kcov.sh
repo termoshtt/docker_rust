1f1e3839e283220568d52a07d52c244f5c149721
Toshiki Teramura <toshiki.teramura@gmail.com>
2018-02-11 19:18:07 +0900

    Use cargo-kcov

--- kcov-test/kcov.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-for file in target/debug/*-*[^\.d]; do
-  mkdir -p "target/cov/$(basename $file)"
-  kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file"
-done
-bash <(curl -s https://codecov.io/bash)
